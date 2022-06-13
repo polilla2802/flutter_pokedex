09f871c291fcb639d7783874e5588e01320d873c
Eduardo Hidalgo <polilla2802@gmail.com>
2022-06-13 00:26:14 -0500

    Changed name to Cubit States

--- lib/app/controllers/cubit/pokedex_cubit.dart
+++ lib/app/controllers/cubit/pokedex_cubit.dart
@@ -9,23 +9,23 @@ part '../state/pokedex_state.dart';
 class PokedexCubit extends Cubit<PokedexState> {
   final PokedexRepo _pokedexRepo;
 
-  PokedexCubit(this._pokedexRepo) : super(const PokedexInitial());
+  PokedexCubit(this._pokedexRepo) : super(const PokemonInitial());
 
   Future<void> getPokemonCount(int pokemonCount, BuildContext context) async {
     try {
-      emit(PokedexLoading());
+      emit(PokemonLoading());
 
       Future.delayed(const Duration(milliseconds: 100), () {
-        emit(PokedexLoaded(pokemonCount));
+        emit(PokemonCount(pokemonCount));
       });
     } catch (e) {
-      emit(PokedexError("error"));
+      emit(PokemonError("error"));
     }
   }
 
   Future<void> getPokemonById(int pokemonId, BuildContext context) async {
     try {
-      emit(PokedexLoading());
+      emit(PokemonLoading());
 
       Result<Pokemon> pokemon = await _pokedexRepo.getPokemonById(pokemonId);
 
@@ -33,7 +33,40 @@ class PokedexCubit extends Cubit<PokedexState> {
       emit(PokemonLoaded(pokemon.data!));
     } catch (e) {
       if (isClosed) return null;
-      emit(PokedexError("error"));
+      emit(PokemonError("error"));
+    }
+  }
+
+  Future<void> getPokemonMoveById(
+      int pokemonMoveId, BuildContext context) async {
+    try {
+      emit(PokemonLoading());
+
+      Result<PokemonMove> pokemonMove =
+          await _pokedexRepo.getPokemonMoveById(pokemonMoveId);
+
+      print("pokemon ${pokemonMove.data}");
+      emit(PokemonMoveLoaded(pokemonMove.data!));
+    } catch (e) {
+      if (isClosed) return null;
+      emit(PokemonError("error"));
+    }
+  }
+
+  Future<void> getPokemonDetailsById(
+      int pokemonId, BuildContext context) async {
+    try {
+      emit(PokemonLoading());
+
+      Result<Pokemon> pokemon = await _pokedexRepo.getPokemonById(pokemonId);
+      Result<PokemonDetails> pokemonDetails =
+          await _pokedexRepo.getPokemonDetailsById(pokemonId);
+
+      print("pokemon ${pokemon.data}");
+      emit(PokemonLoaded(pokemon.data!, pokemonDetails: pokemonDetails.data));
+    } catch (e) {
+      if (isClosed) return null;
+      emit(PokemonError("error"));
     }
   }
 
