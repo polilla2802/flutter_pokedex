c31b1ebcb01495224cb4553c9c783c40cb4c6016
Eduardo Hidalgo <polilla2802@gmail.com>
2022-06-13 17:29:18 -0500

    removed unnecessary dependency

--- lib/app/presentation/components/cards/pokemon_move.dart
+++ lib/app/presentation/components/cards/pokemon_move.dart
@@ -1,5 +1,4 @@
 import 'dart:ui';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pokedex/app/controllers/cubit/pokedex_cubit.dart';
